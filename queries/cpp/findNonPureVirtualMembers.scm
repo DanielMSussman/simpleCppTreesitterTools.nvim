7dbe09fc7b56c1b8a60cc6b72eb22d5f1f4f7026
Daniel M Sussman <dmsussman@gmail.com>
2025-03-10 13:11:10 -0400

    don't get reference and pointer declarations unless they have a function_declarator as a child

--- queries/cpp/findNonPureVirtualMembers.scm
+++ queries/cpp/findNonPureVirtualMembers.scm
@@ -11,8 +11,8 @@
        [
         ;; since I want to be able to add the correct * or & or nothing, explicitly list out the possible declarators
         (function_declarator) @valueReturn
-        (pointer_declarator) @pointerReturn
-        (reference_declarator) @referenceReturn
+        (pointer_declarator (function_declarator)) @pointerReturn ;;only grab pointer (or reference, below) return if a child is a function declaration
+        (reference_declarator (function_declarator)) @referenceReturn
         ] 
        !default_value ;; reject functions with a default_value ("virtual void foo() = 0;")
        ) @functionDeclaration
@@ -23,8 +23,8 @@
        type: (_)* @type ;;class constructor won't have a type, so we need to be able to match on zero or more types
        [
         (function_declarator) @valueReturn
-        (pointer_declarator) @pointerReturn
-        (reference_declarator) @referenceReturn
+        (pointer_declarator (function_declarator)) @pointerReturn
+        (reference_declarator (function_declarator)) @referenceReturn
         ]
        ) @templateOrConstructorDeclaration
      ]
